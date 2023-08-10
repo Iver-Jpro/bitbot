7a3a41fc1cb1bc4228f8e035221a29ece8794ec1
Iver-Jpro <iver@jpro.no>
2023-08-10 12:26:32 +0200

    Litt mer greier

--- backend/usbSerial.py
+++ backend/usbSerial.py
@@ -3,7 +3,7 @@ import serial
 ser = serial.Serial('COM3', 115200)  # Change 'COM3' to the appropriate COM port
 ser.timeout = 10
 
-ser.write(b'Hello, micro:bit!')
+ser.write(b'Hallo Marius!')
 response = ser.read(100)
 print('Received:', response)
 ser.close()
